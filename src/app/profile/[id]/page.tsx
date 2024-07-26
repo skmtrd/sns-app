d85fb0b269aa4747b71f52b6c3e123a313c66778
skmtrd <s1f102400314@iniad.org>
2024-07-27 03:38:08 +0900

    プロフィールページでタグ一覧をフェッチ

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,7 +1,27 @@
 import React from "react";
 
-const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+const fetchTags = async () => {
+  const res = await fetch("http://localhost:3000/api/tag", {
+    cache: "no-cache",
+  });
+
+  const data = await res.json();
+
+  return data.tags;
+};
+
+const ProfilePage = async () => {
+  const tags = await fetchTags();
+  return (
+    <div>
+      <h1>Profile</h1>
+      <ul>
+        {tags.map((tag) => (
+          <li key={tag.id}>{tag.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default ProfilePage;
