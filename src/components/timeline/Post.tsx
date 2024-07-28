a1baa4ad033b590101af8b7d395765269501a807
caru-ini <caru17122@gmail.com>
2024-07-29 01:46:16 +0900

    Add timestamp formatting

--- src/components/timeline/Post.tsx
+++ src/components/timeline/Post.tsx
@@ -1,3 +1,5 @@
+import { formatTime } from "@/lib/formatTime";
+
 type PostProps = {
   username: string;
   timestamp: string;
@@ -15,8 +17,7 @@ export const Post: React.FC<PostProps> = ({
     <div className='w-11/12 rounded-lg bg-white p-4 shadow'>
       <div className='mb-2 flex items-start justify-between'>
         <h3 className='font-bold'>{username}</h3>
-        {/* 〇〇時間前みたいなのは後で実装 */}
-        <p className='text-sm text-gray-500'>{timestamp}</p>
+        <p className='text-sm text-gray-500'>{formatTime(timestamp)}</p>
       </div>
       <p className='mb-2'>{content}</p>
       <div className='flex flex-wrap'>
