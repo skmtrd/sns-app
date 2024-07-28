2376b957b0b59c2ac7f0f4177b2ebe09ea6a984e
skmtrd <s1f102400314@iniad.org>
2024-07-28 17:58:36 +0900

    サイドバーの遷移

--- src/components/layout/SideBar.tsx
+++ src/components/layout/SideBar.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-
+import Link from "next/link";
 import {
   Home,
   BookOpen,
@@ -10,12 +9,16 @@ import {
   TrendingUp,
 } from "lucide-react";
 
-const SideBar = () => {
+type Props = {
+  userId: string;
+};
+
+const SideBar: React.FC<Props> = ({ userId }) => {
   const navItems = [
-    { page: "timeline", label: "タイムライン", icon: Home },
-    { page: "tasks", label: "課題共有", icon: BookOpen },
-    { page: "qa", label: "質問スペース", icon: HelpCircle },
-    { page: "profile", label: "プロフィール", icon: User },
+    { page: "/timeline/all", label: "タイムライン", icon: Home },
+    { page: "/assiignment", label: "課題共有", icon: BookOpen },
+    { page: "/question", label: "質問スペース", icon: HelpCircle },
+    { page: `/profile/${userId}`, label: "プロフィール", icon: User },
   ];
   return (
     <div className="w-20 md:w-80 bg-white p-4 border-r border-gray-200 transition-all duration-300 ease-in-out">
@@ -23,15 +26,16 @@ const SideBar = () => {
         INIAD SNS
       </h1>
       {navItems.map(({ page, label, icon: Icon }) => (
-        <button
+        <Link
           key={page}
+          href={page}
           className="w-full flex items-center justify-center md:justify-start p-2 mb-2 rounded hover:text-blue-600 font-bold"
         >
           <div className="w-10 h-10 flex items-center justify-center">
             <Icon size={24} />
           </div>
           <span className="hidden md:inline md:ml-3">{label}</span>
-        </button>
+        </Link>
       ))}
     </div>
   );
