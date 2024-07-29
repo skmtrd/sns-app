49a1af7483b8d9a758da626b3315e100c68aff4f
riku546 <opa9788@gmail.com>
2024-07-29 16:37:11 +0900

    prisma.tsの型をanyにした

---
+++ src/app/api/__tests__/libTest/checkExitingTag.test.ts
@@ -0,0 +1,10 @@
+import { expect, test, describe } from "@jest/globals";
+import { checkTagExits } from "../../lib/tag/checkExitingTag";
+import "@testing-library/jest-dom";
+
+describe("checkTagExits test", () => {
+  test("should return 200", async () => {
+    expect(await checkTagExits("testtag1")).toBe(true);
+    expect(await checkTagExits("riku")).toBe(false);
+  });
+});
--- src/app/api/lib/tag/checkExitingTag.ts
+++ src/app/api/lib/tag/checkExitingTag.ts
@@ -1,9 +1,11 @@
+import { dbConnect } from "../dbConnect";
 import prisma from "../prisma";
 
 //タグがTagテーブルに存在するかを確認
 export const checkTagExits = async (tagName: string): Promise<boolean> => {
   //prismaでdbを操作しているのに
   //データベース接続していないのは、この関数の呼び出し元で接続しているから
+  dbConnect();
   const existingTag = await prisma.tag.findUnique({
     where: { name: tagName },
   });
