35508c9d9ad7ce8d1b60e8d823d3cd282173c719
caru-ini <caru17122@gmail.com>
2024-07-29 21:06:25 +0900

    Refactor calculate method for formatting

--- src/lib/formatTime.ts
+++ src/lib/formatTime.ts
@@ -1,30 +1,27 @@
 export const formatTime = (timestamp: string, now: Date): string => {
   const target = new Date(timestamp);
+  const delta = now.getTime() - target.getTime();
+
   const diff = {
     year: now.getFullYear() - target.getFullYear(),
     month: now.getMonth() - target.getMonth(),
     day: now.getDate() - target.getDate(),
-    hour: now.getHours() - target.getHours(),
-    minute: now.getMinutes() - target.getMinutes(),
-    second: now.getSeconds() - target.getSeconds(),
-  };
-  const { year, month, day, hour, minute } = {
-    year: target.getFullYear(),
-    month: target.getMonth() + 1,
-    day: target.getDate(),
-    hour: target.getHours(),
-    minute: target.getMinutes(),
+    hour: Math.floor(delta / (1000 * 60 * 60)),
+    minute: Math.floor((delta / (1000 * 60)) % 60),
+    second: Math.floor((delta / 1000) % 60),
   };
 
   if (diff.year > 0) {
-    return `${year}年${month}月${day}日`;
+    return `${target.getFullYear()}年${
+      target.getMonth() + 1
+    }月${target.getDate()}日`;
   } else if (diff.month > 0) {
-    return `${month}月${day}日`;
+    return `${target.getMonth() + 1}月${target.getDate()}日`;
   } else if (diff.day > 0) {
     if (diff.day === 1) {
       return "昨日";
     }
-    return `${day}日`;
+    return `${target.getDate()}日`;
   } else if (diff.hour > 0) {
     return `${diff.hour}時間前`;
   } else if (diff.minute > 0) {
