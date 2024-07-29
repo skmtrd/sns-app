04dab827a3dd9ce40702a7bf12e87db5e9f3f8f8
skmtrd <s1f102400314@iniad.org>
2024-07-30 05:15:10 +0900

    プロフ画面から編集画面までの遷移を追加

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -4,6 +4,7 @@ import { Tag } from "./edit/page";
 import React, { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import { Loader2 } from "lucide-react";
+import Link from "next/link";
 
 export type UserInfo = {
   id: string;
@@ -80,9 +81,11 @@ const ProfilePage = () => {
             ))}
           </div>
         </div>
-        <button className="mt-2 rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50/50">
-          編集
-        </button>
+        <Link href={`${pathname}/edit`}>
+          <button className="mt-2 rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50/50">
+            編集
+          </button>
+        </Link>
       </main>
     </div>
   );
