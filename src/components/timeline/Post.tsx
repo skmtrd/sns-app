b4b408dc83feb467bf7037cfa52c2a9d8cc5361e
caru-ini <caru17122@gmail.com>
2024-07-30 10:30:22 +0900

    Format

--- src/components/timeline/Post.tsx
+++ src/components/timeline/Post.tsx
@@ -1,5 +1,5 @@
-import { formatTime } from "@/lib/formatTime";
-import { useEffect, useState } from "react";
+import { formatTime } from '@/lib/formatTime';
+import { useEffect, useState } from 'react';
 
 type PostProps = {
   username: string;
@@ -8,12 +8,7 @@ type PostProps = {
   tags?: string[];
 };
 
-export const Post: React.FC<PostProps> = ({
-  username,
-  timestamp,
-  content,
-  tags,
-}) => {
+export const Post: React.FC<PostProps> = ({ username, timestamp, content, tags }) => {
   const [time, setTime] = useState(new Date());
   useEffect(() => {
     const updateDate = setInterval(() => {
@@ -23,18 +18,18 @@ export const Post: React.FC<PostProps> = ({
   }, []);
 
   return (
-    <div className="w-11/12 rounded-lg bg-white p-4 shadow">
-      <div className="mb-2 flex items-start justify-between">
-        <h3 className="font-bold">{username}</h3>
-        <p className="text-sm text-gray-500">{formatTime(timestamp, time)}</p>
+    <div className='w-11/12 rounded-lg bg-white p-4 shadow'>
+      <div className='mb-2 flex items-start justify-between'>
+        <h3 className='font-bold'>{username}</h3>
+        <p className='text-sm text-gray-500'>{formatTime(timestamp, time)}</p>
       </div>
-      <p className="mb-2">{content}</p>
-      <div className="flex flex-wrap">
+      <p className='mb-2'>{content}</p>
+      <div className='flex flex-wrap'>
         {tags &&
           tags.map((tag, index) => (
             <span
               key={index}
-              className="mb-2 mr-2 rounded-full bg-blue-100 px-2 py-1 text-xs text-blue-800"
+              className='mb-2 mr-2 rounded-full bg-blue-100 px-2 py-1 text-xs text-blue-800'
             >
               #{tag}
             </span>
