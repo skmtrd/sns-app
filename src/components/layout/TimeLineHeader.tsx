58c7f952623a85ef6d3656fb78005944ff7e5892
skmtrd <skmtgame23@gmail.com>
2024-08-03 20:51:31 +0900

    レスポンシブとヘッダーを固定

--- src/components/layout/TimeLineHeader.tsx
+++ src/components/layout/TimeLineHeader.tsx
@@ -4,12 +4,14 @@ type TimeLineHeaderProps = {
 
 const TimeLineHeader: React.FC<TimeLineHeaderProps> = ({ target }) => {
   return (
-    <header className='flex w-full items-center justify-between border-b border-gray-200 bg-white p-4'>
-      <h2 className='text-xl font-bold'>タイムライン-{target}</h2>
-      <div className='flex items-center'>
-        <div className='relative mr-4'></div>
-      </div>
-    </header>
+    <div className='relative w-full'>
+      <header className='fixed flex w-full items-center justify-between border-b border-gray-200 bg-white p-4'>
+        <h2 className='text-xl font-bold'>タイムライン-{target}</h2>
+        <div className='flex items-center'>
+          <div className='relative mr-4'></div>
+        </div>
+      </header>
+    </div>
   );
 };
 
