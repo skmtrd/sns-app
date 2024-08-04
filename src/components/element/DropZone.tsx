83d40023385a3c66391ff5e87c928156e2bf8895
caru-ini <caru17122@gmail.com>
2024-08-04 16:49:47 +0900

    Update Dropzone to show current image

--- src/components/element/DropZone.tsx
+++ src/components/element/DropZone.tsx
@@ -6,9 +6,11 @@ import { useDropzone } from 'react-dropzone';
 type DropZoneProps = {
   onDrop: (files: File[]) => void;
   className?: string;
+  status?: 'uploading' | 'success' | 'error' | 'idle';
+  value?: string;
 };
 
-export const Dropzone: React.FC<DropZoneProps> = ({ onDrop, className }) => {
+export const Dropzone: React.FC<DropZoneProps> = ({ onDrop, className, status, value }) => {
   const [files, setFiles] = useState<File[]>([]);
   const { getRootProps, getInputProps, isDragActive, isDragReject, acceptedFiles } = useDropzone({
     accept: { 'image/*': ['.jpg', '.jpeg', '.png', '.gif', '.webp'] },
@@ -32,7 +34,14 @@ export const Dropzone: React.FC<DropZoneProps> = ({ onDrop, className }) => {
         isDragActive ? 'border-blue-500' : '',
       )}
     >
-      {!acceptedFiles.length ? (
+      {!acceptedFiles.length && value ? (
+        <div className='flex flex-col items-center'>
+          <p>現在の画像</p>
+          <p>変更する場合は新しい画像をドロップまたはこのエリアをクリック</p>
+          <Image src={value} alt='uploaded image' width={200} height={200} />
+        </div>
+      ) : !acceptedFiles.length ? (
+        // if no files are selected or set
         <div>
           <input {...getInputProps()} />
           <p className='font-bold'>ファイルをドロップ</p>
@@ -42,24 +51,34 @@ export const Dropzone: React.FC<DropZoneProps> = ({ onDrop, className }) => {
           </button>
         </div>
       ) : isDragReject ? (
+        // if file is rejected
         <p className='text-red-500'>画像ファイルのみアップロード可能です</p>
       ) : (
+        // if file is accepted
         files.map((file) => (
           <div key={file.name} className='flex flex-col items-center'>
             <div className='flex w-full items-center justify-between'>
               <p>
                 {file.name} ({Math.ceil(file.size / 1024)}KB)
               </p>
-              <button
-                type='button'
-                className='rounded-md px-2 text-red-500 hover:bg-red-100 hover:text-red-700'
-                onClick={(e) => {
-                  setFiles([]);
-                  acceptedFiles.splice(acceptedFiles.indexOf(file), 1);
-                }}
-              >
-                削除
-              </button>
+              {status == 'uploading' ? (
+                <div className='size-6 animate-spin rounded-full border-b-2 border-gray-900' />
+              ) : (
+                <button
+                  type='button'
+                  className='rounded-md px-2 text-red-500 hover:bg-red-100 hover:text-red-700'
+                  onClick={() => {
+                    if (status == 'error') {
+                      onDrop(acceptedFiles);
+                      return;
+                    }
+                    setFiles([]);
+                    acceptedFiles.splice(acceptedFiles.indexOf(file), 1);
+                  }}
+                >
+                  {status == 'error' ? '再試行' : '削除'}
+                </button>
+              )}
             </div>
             <Image
               src={URL.createObjectURL(file)}
