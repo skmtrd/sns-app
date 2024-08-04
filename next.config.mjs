5c96112ea68b02789ca90e5889d9a42a54ae2592
caru-ini <caru17122@gmail.com>
2024-08-04 18:38:56 +0900

    Update nextjs config to accept img.clerk.com

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,12 @@ const nextConfig = {
         port: '',
         pathname: '**',
       },
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+        port: '',
+        pathname: '**',
+      },
     ],
   },
 };
