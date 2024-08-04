1ec1e7b174fff19e36dc414c602e03e5deeceaf8
caru-ini <caru17122@gmail.com>
2024-08-04 16:46:32 +0900

    Update s3 Client

--- src/app/api/lib/S3Client.ts
+++ src/app/api/lib/S3Client.ts
@@ -1,11 +1,16 @@
-import { GetObjectCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import {
+  DeleteObjectCommand,
+  GetObjectCommand,
+  PutObjectCommand,
+  S3Client,
+} from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import z from 'zod';
 
 const credentials = z
   .object({
-    accessKeyId: z.string().min(1),
-    secretAccessKey: z.string().min(1),
+    accessKeyId: z.string({ required_error: 'S3_ACCESS_KEY_ID is required' }).min(1),
+    secretAccessKey: z.string({ required_error: 'S3 SECRET_ACCESS_KEY is required' }).min(1),
   })
   .parse({
     accessKeyId: process.env.S3_ACCESS_KEY_ID,
@@ -17,21 +22,20 @@ const S3URL = z.string().regex(regex).parse(process.env.S3_URL);
 const endpoint = z.string().min(1).parse(S3URL.match(regex)?.[1]);
 const bucketName = z.string().min(1).parse(S3URL.match(regex)?.[2]);
 
-console.log('endpoint:', endpoint);
-console.log('bucketName:', bucketName);
-
 export const client = new S3Client({
   region: 'APAC',
   endpoint,
   credentials,
 });
 
-export const putObject = async (key: string, body: Buffer) => {
+export const putObject = async (key: string, body: Buffer, contentType?: string) => {
   await client.send(
     new PutObjectCommand({
       Bucket: bucketName,
       Key: key,
       Body: body,
+      ContentType: contentType,
+      Expires: new Date(),
     }),
   );
 };
@@ -42,4 +46,13 @@ export const getObjectURL = async (key: string) => {
     Key: key,
   });
   return await getSignedUrl(client, command);
+};
+
+export const deleteObject = async (key: string) => {
+  await client.send(
+    new DeleteObjectCommand({
+      Bucket: bucketName,
+      Key: key,
+    }),
+  );
 };
