ab6bf14cfc79daf43b9b9afde152690579694a8b
caru-ini <caru17122@gmail.com>
2024-08-04 16:48:05 +0900

    Update next config to accept s3 url

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.r2.cloudflarestorage.com',
+        port: '',
+        pathname: '**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
