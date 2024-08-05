5b8b9e22de2d92ef398f243fa6fca865c2f26633
skmtrd <skmtgame23@gmail.com>
2024-08-05 18:21:50 +0900

    微調整

--- src/components/question/QuestionPost.tsx
+++ src/components/question/QuestionPost.tsx
@@ -4,13 +4,13 @@ const QuestionPost = ({ post }) => {
       <div className='flex items-start justify-between'>
         <div>
           <h3 className='text-lg font-bold'>{post.title}</h3>
-          <p className='text-sm text-gray-600'>{post.author.name}</p>
+          <p className='text-sm text-gray-600'>{post.author.name}さん</p>
           <p className='mt-2'>{post.description}</p>
         </div>
         <p className='mr-1 text-sm text-gray-500'>{post.createdAt}</p>
       </div>
       <div className='mt-4'>
-        <h4 className='font-semibold'>回答(0)</h4>
+        <h4 className='font-semibold'>回答</h4>
         <div className='mt-2 border-l-2 border-blue-300 pl-4'>
           <p className='text-sm text-gray-600'>{post.answers[0].name}さんの回答:</p>
           <p>{post.answers[0].content}</p>
