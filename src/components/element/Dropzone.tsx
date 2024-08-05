30e2944d8ec4fc0e4a7d5cfbf7c875220d15bc38
caru-ini <caru17122@gmail.com>
2024-08-05 14:39:42 +0900

    Update Dropzone

--- src/components/element/Dropzone.tsx
+++ src/components/element/Dropzone.tsx
@@ -1,15 +1,15 @@
 import { cn } from '@/lib/utils';
+import { Upload } from 'lucide-react';
 import Image from 'next/image';
 import { useCallback, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
 
-type DropZoneProps = {
+type DropzoneProps = {
   onDrop: (files: File[]) => void;
   className?: string;
-  value?: string;
 };
 
-export const Dropzone: React.FC<DropZoneProps> = ({ onDrop, className, value }) => {
+export const Dropzone: React.FC<DropzoneProps> = ({ onDrop, className }) => {
   const [files, setFiles] = useState<File[]>([]);
   const { getRootProps, getInputProps, isDragActive, isDragReject, acceptedFiles } = useDropzone({
     accept: { 'image/*': ['.jpg', '.jpeg', '.png', '.gif', '.webp'] },
@@ -28,21 +28,16 @@ export const Dropzone: React.FC<DropZoneProps> = ({ onDrop, className, value }) 
     <div
       {...getRootProps()}
       className={cn(
-        'rounded-md border-2 border-dashed border-gray-300 p-4 text-center',
+        'max-h-full max-w-full rounded-md border-2 border-dashed border-gray-300 p-4 text-center',
         className,
         isDragActive ? 'border-blue-500' : '',
       )}
     >
-      {!acceptedFiles.length && value ? (
-        <div className='flex flex-col items-center'>
-          <p>現在の画像</p>
-          <p>変更する場合は新しい画像をドロップまたはこのエリアをクリック</p>
-          <Image src={value} alt='uploaded image' width={200} height={200} />
-        </div>
-      ) : !acceptedFiles.length ? (
+      {!acceptedFiles.length ? (
         // if no files are selected or set
-        <div>
+        <div className='flex flex-col items-center gap-y-2'>
           <input {...getInputProps()} />
+          <Upload size='50' />
           <p className='font-bold'>ファイルをドロップ</p>
           <p>または</p>
           <button type='button' className='rounded bg-blue-500 px-4 py-2 text-white'>
@@ -55,28 +50,23 @@ export const Dropzone: React.FC<DropZoneProps> = ({ onDrop, className, value }) 
       ) : (
         // if file is accepted
         files.map((file) => (
-          <div key={file.name} className='flex flex-col items-center'>
-            <div className='flex w-full items-center justify-between'>
-              <p>
-                {file.name} ({Math.ceil(file.size / 1024)}KB)
-              </p>
-              <button
-                type='button'
-                className='rounded-md px-2 text-red-500 hover:bg-red-100 hover:text-red-700'
-                onClick={() => {
-                  setFiles([]);
-                  acceptedFiles.splice(acceptedFiles.indexOf(file), 1);
-                }}
-              >
-                削除
-              </button>
-            </div>
+          <div key={file.name} className='relative flex flex-col items-center'>
+            <button
+              type='button'
+              className='absolute right-0 top-0 rounded-md px-2 py-0.5 text-red-500 hover:bg-red-400/30 hover:text-red-700'
+              onClick={() => {
+                setFiles([]);
+                acceptedFiles.splice(acceptedFiles.indexOf(file), 1);
+              }}
+            >
+              削除
+            </button>
             <Image
               src={URL.createObjectURL(file)}
               alt={file.name}
-              className='size-32 object-cover'
               width={400}
               height={400}
+              layout='responsive'
             />
           </div>
         ))
