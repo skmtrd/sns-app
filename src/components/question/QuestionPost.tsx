b69895c09480ce3f980468691f947a4f6ca21625
skmtrd <skmtgame23@gmail.com>
2024-08-06 22:39:49 +0900

    複数回答に対応

--- src/components/question/QuestionPost.tsx
+++ src/components/question/QuestionPost.tsx
@@ -2,8 +2,10 @@
 
 import { formatTime } from '@/lib/formatTime';
 import { Reply } from '@/lib/types';
-import { useEffect, useState } from 'react';
+import { ChevronDown, ChevronUp } from 'lucide-react';
+import { useEffect, useRef, useState } from 'react';
 import QuestionReply from './QuestionReply';
+
 type QuestionPostProps = {
   username: string;
   clerkId: string;
@@ -14,6 +16,7 @@ type QuestionPostProps = {
   description: string;
   replies: Reply[];
 };
+
 const QuestionPost: React.FC<QuestionPostProps> = ({
   username,
   clerkId,
@@ -25,6 +28,13 @@ const QuestionPost: React.FC<QuestionPostProps> = ({
   replies,
 }) => {
   const [time, setTime] = useState(new Date());
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [contentHeight, setContentHeight] = useState(0);
+  const contentRef = useRef<HTMLDivElement>(null);
+
+  const handleDrawerToggle = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
 
   useEffect(() => {
     const updateDate = setInterval(() => {
@@ -36,6 +46,12 @@ const QuestionPost: React.FC<QuestionPostProps> = ({
     };
   }, []);
 
+  useEffect(() => {
+    if (contentRef.current) {
+      setContentHeight(contentRef.current.scrollHeight);
+    }
+  }, [replies]);
+
   return (
     <div key={id} className='w-11/12 rounded-lg bg-white p-6 shadow'>
       <div className='flex items-start justify-between'>
@@ -46,8 +62,33 @@ const QuestionPost: React.FC<QuestionPostProps> = ({
         </div>
         <p className='mr-1 text-sm text-gray-500'>{formatTime(timestamp, time)}</p>
       </div>
+      <h4 className='mt-4 font-semibold'>回答({replies.length})</h4>
       {replies.length > 0 ? (
-        <QuestionReply reply={replies[0]} />
+        <>
+          <QuestionReply reply={replies[0]} amountOfReply={replies.length} />
+          {replies.length > 1 && (
+            <>
+              <div
+                ref={contentRef}
+                className='overflow-hidden transition-all duration-500 ease-in-out'
+                style={{ height: isDrawerOpen ? `${contentHeight}px` : '0' }}
+              >
+                {replies.slice(1).map((reply, index) => (
+                  <QuestionReply key={index} reply={reply} amountOfReply={replies.length} />
+                ))}
+              </div>
+              <div className='mt-2 flex w-full justify-end'>
+                <button
+                  onClick={handleDrawerToggle}
+                  className='flex items-center justify-center rounded px-2 py-1 text-blue-600 transition-colors hover:bg-blue-200'
+                >
+                  {isDrawerOpen ? <ChevronUp className='mr-1' /> : <ChevronDown className='mr-1' />}
+                  <p>{isDrawerOpen ? '閉じる' : 'もっと見る'}</p>
+                </button>
+              </div>
+            </>
+          )}
+        </>
       ) : (
         <p className='mt-4 font-semibold text-red-500'>まだ回答がありません</p>
       )}
--- src/components/question/QuestionReply.tsx
+++ src/components/question/QuestionReply.tsx
@@ -1,9 +1,13 @@
-import type { Reply } from '@/lib/types';
+import { Reply } from '@/lib/types';
 
-const QuestionReply: React.FC<Reply> = ({ reply }) => {
+type QuestionReplyProps = {
+  reply: Reply;
+  amountOfReply: number;
+};
+
+const QuestionReply: React.FC<QuestionReplyProps> = ({ reply, amountOfReply }) => {
   return (
     <div className='mt-4'>
-      <h4 className='font-semibold'>回答</h4>
       <div className='mt-2 border-l-2 border-blue-300 pl-4'>
         <p className='text-sm text-gray-600'>{reply.author.name}さんの回答:</p>
         <p>{reply.content}</p>
