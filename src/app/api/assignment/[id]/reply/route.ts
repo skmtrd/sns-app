0e8eb0563da8a830f445cf425de9f22d7a1c487f
k-arikawa0312 <s1F102402710@iniad.org>
2024-08-07 03:45:45 +0900

    userIdが同じならリプ飛ばせる

---
+++ src/app/api/assignment/[id]/reply/route.ts
@@ -0,0 +1,38 @@
+import { dbConnect } from '@/app/api/lib/dbConnect';
+import { handleAPIError } from '@/app/api/lib/handleAPIError';
+import prisma from '@/app/api/lib/prisma';
+import { apiRes } from '@/app/api/types';
+import { NextResponse } from 'next/server';
+
+export const POST = async (req: Request, res: NextResponse) =>
+  handleAPIError(async () => {
+    dbConnect();
+
+    //がんばれK
+    //成長するんだ
+
+    const { content, assignmentId } = await req.json();
+
+    const userId = 'user_2kAm1CqUROhV77wXS43Td3lI3NN';
+
+    const user = await prisma.user.findUniqueOrThrow({
+      where: { clerkId: userId },
+    });
+
+    const reply = await prisma.assignmentReply.create({
+      data: {
+        assignment: {
+          connect: { id: assignmentId },
+        },
+        content,
+        author: {
+          connect: { id: user.id },
+        },
+      },
+      include: {
+        author: true,
+      },
+    });
+
+    return NextResponse.json<apiRes>({ message: 'success', data: reply }, { status: 200 });
+  });
