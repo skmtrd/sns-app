1460237109bf2d4916b6f666530ea3d421894a07
caru-ini <caru17122@gmail.com>
2024-08-07 11:22:48 +0900

    Update pages to use useData hook

--- src/app/question/all/page.tsx
+++ src/app/question/all/page.tsx
@@ -2,53 +2,50 @@
 import Header from '@/components/element/Header';
 import FixedHeader from '@/components/layout/FixedHeader';
 import QuestionPost from '@/components/question/QuestionPost';
-import { Loader2 } from 'lucide-react';
-import useSWR from 'swr';
+import useData from '@/hooks/useData';
+import { LoaderCircle } from 'lucide-react';
 import { z } from 'zod';
 
-export const questionSchema = z.object({
-  author: z.object({
-    name: z.string(),
+export const questionSchema = z
+  .object({
+    author: z.object({
+      name: z.string(),
+      id: z.string(),
+      clerkId: z.string(),
+      tags: z.array(z.object({ name: z.string(), id: z.string() })),
+    }),
+    createdAt: z.string(),
     id: z.string(),
-    clerkId: z.string(),
-    tags: z.array(z.object({ name: z.string(), id: z.string() })),
-  }),
-  createdAt: z.string(),
-  id: z.string(),
-  title: z.string(),
-  description: z.string(),
-  replies: z.array(
-    z.object({ id: z.string(), content: z.string(), author: z.object({ name: z.string() }) }),
-  ),
-});
+    title: z.string(),
+    description: z.string(),
+    replies: z.array(
+      z.object({ id: z.string(), content: z.string(), author: z.object({ name: z.string() }) }),
+    ),
+  })
+  .array();
 
 const TimelineAll = () => {
-  const fetcher = (url: string) => fetch(url).then((res) => res.json());
-  const { data, error, isLoading } = useSWR('/api/question', fetcher, {
-    refreshInterval: 20000,
-    revalidateOnFocus: true,
-  });
+  const { data, error, isLoading } = useData('/api/question', questionSchema);
 
-  if (isLoading) {
-    return (
-      <div className='flex h-svh w-full flex-1 grow flex-col items-center justify-center gap-4 bg-gray-100'>
-        <Loader2 size='64' className='animate-spin text-blue-600' />
-        ロード中...
-      </div>
-    );
-  }
   if (error) {
     return <div>Error</div>;
   }
 
-  const qestions = questionSchema.array().parse(data.data);
+  if (isLoading || !data) {
+    return (
+      <div className='flex h-svh w-full flex-1 grow flex-col items-center justify-center gap-4 bg-gray-100'>
+        <LoaderCircle size='64' className='animate-spin text-blue-600' />
+        ロード中...
+      </div>
+    );
+  }
 
   return (
     <div className='flex w-full flex-1 grow flex-col items-center gap-4 overflow-y-scroll bg-gray-100'>
       <FixedHeader title={'質問'} target={'すべて'} />
       <Header title={''} />
       <div className='flex w-full grow flex-col items-center gap-y-4 p-3'>
-        {qestions.map((question, index) => (
+        {data.map((question, index) => (
           <QuestionPost
             key={index}
             title={question.title}
