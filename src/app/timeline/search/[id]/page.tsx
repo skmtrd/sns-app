9cd4a22dbd496bdb3021826db05dcbf317abf741
skmtrd <skmtgame23@gmail.com>
2024-08-13 00:22:58 +0900

    微調整

--- src/app/timeline/search/[id]/page.tsx
+++ src/app/timeline/search/[id]/page.tsx
@@ -5,9 +5,36 @@ import { Post } from '@/components/timeline/Post';
 import useData from '@/hooks/useData';
 import { LoaderCircle } from 'lucide-react';
 import { usePathname } from 'next/navigation';
-import { postSchema } from '../../all/page';
+import { toHiragana } from 'wanakana';
+import { z } from 'zod';
 
-const TimelineAll = () => {
+export const postSchema = z
+  .object({
+    author: z.object({
+      name: z.string(),
+      id: z.string(),
+      clerkId: z.string(),
+      tags: z.array(z.object({ name: z.string(), id: z.string() })),
+      introduction: z.string(),
+    }),
+    createdAt: z.string(),
+    id: z.string(),
+    content: z.string(),
+    avatar: z.string(),
+    likes: z.array(
+      z.object({ author: z.object({ name: z.string(), clerkId: z.string(), id: z.string() }) }),
+    ),
+    replies: z.array(
+      z.object({
+        id: z.string(),
+        content: z.string(),
+        author: z.object({ name: z.string(), id: z.string(), clerkId: z.string() }),
+      }),
+    ),
+  })
+  .array();
+
+const SearchedTimeline = () => {
   const pathName = usePathname();
   const searchedWord = decodeURIComponent(pathName.split('/search/')[1]).trim();
   const searchWords = searchedWord.split(' ').filter((term) => term.trim() !== '');
@@ -42,24 +69,28 @@ const TimelineAll = () => {
       <FixedHeader title={'検索'} target={searchedWord} />
       <Header title={''} />
       <div className='flex w-full grow flex-col items-center gap-y-4 p-3'>
-        {filteredPosts.map((post, index) => (
-          <Post
-            key={index}
-            username={post.author.name}
-            clerkId={post.author.clerkId}
-            id={post.author.id}
-            timestamp={post.createdAt}
-            content={post.content}
-            tags={post.author.tags}
-            postId={post.id}
-            avatar={post.avatar}
-            likes={post.likes}
-            replies={post.replies}
-          />
-        ))}
+        {filteredPosts.length === 0 ? (
+          <p>検索結果に一致するポストがありません</p>
+        ) : (
+          filteredPosts.map((post, index) => (
+            <Post
+              key={index}
+              username={post.author.name}
+              clerkId={post.author.clerkId}
+              id={post.author.id}
+              timestamp={post.createdAt}
+              content={post.content}
+              tags={post.author.tags}
+              postId={post.id}
+              avatar={post.avatar}
+              likes={post.likes}
+              replyCount={post.replies.length}
+            />
+          ))
+        )}
       </div>
     </div>
   );
 };
 
-export default TimelineAll;
+export default SearchedTimeline;
