645327725c2f2baae9914f55067e0b015e2591d9
skmtrd <skmtgame23@gmail.com>
2024-08-16 20:35:04 +0900

    不要なコメントアウトの削除

--- src/app/api/profile/[id]/route.ts
+++ src/app/api/profile/[id]/route.ts
@@ -54,13 +54,9 @@ export const PUT = async (req: Request, res: NextResponse) =>
 
     const { name, introduction, userId } = await req.json();
 
-    //新しく登録するuserIdが存在するかを確認する
-    //存在したら、エラーメッセージを返す
-
     const isUserIdExists = await checkUserIdExists(userId, clerkId);
     if (isUserIdExists) {
       return NextResponse.json<apiRes>({ message: 'userId already exits' }, { status: 404 });
-      //userIdが存在しなければ、新しいプロフィールを作成する
     } else {
       const newProfile = await prisma.user.update({
         data: { name, introduction, id: userId },
