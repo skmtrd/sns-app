0f40e3fa3422011955ce5be621708f72881d57d5
skmtrd <skmtgame23@gmail.com>
2024-09-04 14:02:59 +0900

    登録課題も新構造

--- src/app/my-assignments/page.tsx
+++ src/app/my-assignments/page.tsx
@@ -1,14 +1,11 @@
 'use client';
-import AssignmentPost from '@/components/assignmentshare/AssignmentPost';
-import Header from '@/components/element/Header';
-import FixedHeader from '@/components/layout/FixedHeader';
+import AssignmnetSharePage from '@/components/assignmentshare/AssignmnetSharePage';
 import QuestionSkeltonLoading from '@/components/loading/QuestionSkeltonLoading';
 import useData from '@/hooks/useData';
-import { deleteAssignment } from '@/lib/deleteRequests';
-import { assignmentshareSchema } from '@/lib/schemas';
-import { scrollToTop } from '@/lib/scrollToTop';
+import { AssignmentSchema } from '@/lib/schemas';
 import { useAuth } from '@clerk/nextjs';
 import { useSWRConfig } from 'swr';
+import { z } from 'zod';
 
 const AssignmentAll = () => {
   const { mutate } = useSWRConfig();
@@ -18,7 +15,7 @@ const AssignmentAll = () => {
     data: assignments,
     error,
     isLoading,
-  } = useData(`/api/like/assignment/${currentClerkId}`, assignmentshareSchema);
+  } = useData(`/api/assignment`, z.array(AssignmentSchema));
 
   if (isLoading) {
     return <QuestionSkeltonLoading title={'課題共有'} subtitle={'すべて'} />;
@@ -27,59 +24,16 @@ const AssignmentAll = () => {
     return <div>Error</div>;
   }
 
-  const handleDeleteAssignment = async (
-    e: React.MouseEvent<HTMLButtonElement>,
-    assignmentId: string,
-  ) => {
-    e.stopPropagation();
-    if (!assignments) return;
-    const optimisticData = assignments.filter((assignment) => assignment.id !== assignmentId);
-    try {
-      await mutate(
-        `/api/like/assignment/${currentClerkId}`,
-        async () => {
-          await deleteAssignment(assignmentId);
-          return optimisticData;
-        },
-        {
-          optimisticData,
-          revalidate: false,
-          populateCache: true,
-          rollbackOnError: true,
-        },
-      );
-    } catch (error) {
-      console.error('Failed to delete post:', error);
-    }
-  };
-
+  const filteredAssignments = assignments.filter((assignment) =>
+    assignment.likes.some((like) => like.user.clerkId === currentClerkId),
+  );
   return (
-    <div
-      id='mainContent'
-      className='flex w-full flex-1 grow flex-col items-center gap-4 overflow-y-scroll bg-gray-100'
-    >
-      <FixedHeader title={'登録した課題'} target={null} scrollToTop={scrollToTop} />
-      <Header title={''} />
-      <div className='flex w-full grow flex-col items-center gap-y-1 p-3'>
-        {assignments.map((assignment) => (
-          <AssignmentPost
-            key={assignment.id}
-            assignmentId={assignment.id}
-            title={assignment.title}
-            description={assignment.description}
-            deadline={assignment.deadLine}
-            likes={assignment.likes}
-            timestamp={assignment.createdAt}
-            assignmentAuthorId={assignment.authorId}
-            assignmentAuthorName={assignment.author.name}
-            assignmentAuthorClerkId={assignment.author.clerkId}
-            assignmentAuthorIntroduction={assignment.author.introduction}
-            handleDeleteAssignment={handleDeleteAssignment}
-            currentClerkId={currentClerkId}
-          />
-        ))}
-      </div>
-    </div>
+    <AssignmnetSharePage
+      currentClerkId={currentClerkId}
+      assignments={filteredAssignments}
+      title={'登録した課題'}
+      target={null}
+    />
   );
 };
 
