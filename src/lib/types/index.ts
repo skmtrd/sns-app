298b4b942835c04579dd35dae80a3e50d5ba3a74
skmtrd <skmtgame23@gmail.com>
2024-11-30 03:04:39 +0900

    a4

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -22,31 +22,3 @@ export type Tag = {
   name: string;
 };
 
-// export type Reply = {
-//   id: string;
-//   createdAt: string;
-//   author: {
-//     id: string;
-//     clerkId: string;
-//     name: string;
-//     introduction?: string | undefined;
-//     tags?: { id: string; name: string }[] | undefined;
-//   };
-//   avatar: string | null;
-//   content: string;
-//   parentReplyId: string | null;
-// };
-
-export type QuestionReply = {
-  id: string;
-  createdAt: string;
-  author: {
-    id: string;
-    clerkId: string;
-    name: string;
-    introduction?: string | undefined;
-    tags?: { id: string; name: string }[] | undefined;
-  };
-  content: string;
-  parentReplyId: string | null;
-};
