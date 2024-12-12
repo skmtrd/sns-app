4252974f8fc01aaa2184cbd6b7613dc49a7810eb
skmtrd <skmtgame23@gmail.com>
2024-12-12 16:54:51 +0900

    test

--- src/app/api/cron-job/route.ts
+++ src/app/api/cron-job/route.ts
@@ -1,4 +1,3 @@
-import { formatTimeUntil } from '@/lib/formatTimeUntil';
 import { NextResponse } from 'next/server';
 import { handleAPIError } from '../lib/handleAPIError';
 import prisma from '../lib/prisma';
@@ -27,24 +26,24 @@ export const GET = async (req: Request, res: NextResponse) =>
       },
       orderBy: { createdAt: 'desc' },
     });
-    for (const assignment of assignments) {
-      const [datePart, timePart] = assignment.deadLine.split('/');
-      const [year, month, day] = datePart.split('-').map(Number);
-      const [hour, minute] = timePart.split(':').map(Number);
-      const target = new Date(year, month - 1, day, hour, minute);
-      const timeUntil = formatTimeUntil(target);
-      if (timeUntil === 'over') {
-        const deletedReplies = await prisma.assignmentReply.deleteMany({
-          where: { assignmentId: assignment.id },
-        });
-        const deletedLikes = await prisma.like.deleteMany({
-          where: { assignmentId: assignment.id },
-        });
-        const deletedAssignment = await prisma.assignment.delete({
-          where: { id: assignment.id },
-        });
-      }
-    }
+    // for (const assignment of assignments) {
+    //   const [datePart, timePart] = assignment.deadLine.split('/');
+    //   const [year, month, day] = datePart.split('-').map(Number);
+    //   const [hour, minute] = timePart.split(':').map(Number);
+    //   const target = new Date(year, month - 1, day, hour, minute);
+    //   const timeUntil = formatTimeUntil(target);
+    //   if (timeUntil === 'over') {
+    //     const deletedReplies = await prisma.assignmentReply.deleteMany({
+    //       where: { assignmentId: assignment.id },
+    //     });
+    //     const deletedLikes = await prisma.like.deleteMany({
+    //       where: { assignmentId: assignment.id },
+    //     });
+    //     const deletedAssignment = await prisma.assignment.delete({
+    //       where: { id: assignment.id },
+    //     });
+    //   }
+    // }
 
     return NextResponse.json<apiRes>({ message: 'success', data: assignments }, { status: 200 });
   });
