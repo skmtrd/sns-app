e4e83e79c60287fceb1a1132848ba57bb1dacb6e
skmtrd <skmtgame23@gmail.com>
2024-12-12 17:47:43 +0900

    hhihuyuyuivyubbyu

--- src/app/api/delete-assignment/route.ts
+++ src/app/api/delete-assignment/route.ts
@@ -1,70 +1,12 @@
-'use server';
+import { revalidatePath } from 'next/cache';
 import { NextResponse } from 'next/server';
 import { handleAPIError } from '../lib/handleAPIError';
 import prisma from '../lib/prisma';
 import { apiRes } from '../types';
 
-// export const GET = async (req: Request, res: NextResponse) =>
-//   handleAPIError(async () => {
-//     const assignments = await prisma.assignment.findMany({
-//       include: {
-//         replies: {
-//           include: {
-//             author: true,
-//             childReplies: true,
-//           },
-//         },
-//         likes: {
-//           include: {
-//             user: true,
-//           },
-//         },
-//         author: {
-//           include: {
-//             tags: true,
-//           },
-//         },
-//       },
-//       orderBy: { createdAt: 'desc' },
-//     });
-
-//     // const overDeadlinedAssignments = assignments.filter((assignment) => {
-//     //   const [datePart, timePart] = assignment.deadLine.split('/');
-//     //   const [year, month, day] = datePart.split('-').map(Number);
-//     //   const [hour, minute] = timePart.split(':').map(Number);
-//     //   const target = new Date(year, month - 1, day, hour, minute);
-//     //   const now = new Date();
-//     //   return target < now;
-//     // });
-
-//     const now = new Date();
-
-//     const time = assignments[0].deadLine;
-
-//     const [datePart, timePart] = time.split('/');
-//     const [year, month, day] = datePart.split('-').map(Number);
-//     const [hour, minute] = timePart.split(':').map(Number);
-//     const target = new Date(year, month - 1, day, hour, minute);
-
-//     const result = {
-//       now,
-//       target,
-//       result: target < now,
-//     };
-
-//     const deletedAssignments = await prisma.assignment.deleteMany({
-//       where: {
-//         id: {
-//           in: overDeadlinedAssignments.map((assignment) => assignment.id),
-//         },
-//       },
-//     });
-
-//     return NextResponse.json<apiRes>({ message: 'success', data: result }, { status: 200 });
-//   });
-
 export const GET = async (req: Request, res: NextResponse) =>
   handleAPIError(async () => {
+    revalidatePath('/api/assignment');
     const assignments = await prisma.assignment.findMany({
       include: {
         replies: {
