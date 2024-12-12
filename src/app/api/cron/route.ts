7d5ea5a2e475e1409beeb31232042a92143a6f76
skmtrd <skmtgame23@gmail.com>
2024-12-12 15:21:43 +0900

    Remove cron configuration and refactor API cron route to simplify response handling

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,32 +1,16 @@
-import axios from 'axios';
 import { NextResponse } from 'next/server';
 import { handleAPIError } from '../lib/handleAPIError';
-import prisma from '../lib/prisma';
 import { apiRes } from '../types';
-
-const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL;
 
 export const GET = async (req: Request, res: NextResponse) =>
   handleAPIError(async () => {
-    const assignments = await prisma.assignment.findMany();
+    // const assignments = await prisma.assignment.findMany();
+    // for (const assignment of assignments) {
+    //   const [datePart, timePart] = assignment.deadLine.split('/');
+    //   const [year, month, date] = datePart.split('-');
 
+    // }
     const now = new Date();
-    const oneHourLater = new Date(now.getTime() + 60 * 60 * 1000);
 
-    const filteredAssignments = assignments.filter((assignment) => {
-      const [datePart, timePart] = assignment.deadLine.split('/');
-      const dateTimeString = `${datePart}T${timePart}:00Z`;
-      const deadLineDate = new Date(dateTimeString);
-
-      return deadLineDate <= oneHourLater;
-    });
-
-    for (const assignment of filteredAssignments) {
-      const message = {
-        content: `課題「${assignment.title}」の提出期限が迫っています。締切は${assignment.deadLine}です。`,
-      };
-      if (!DISCORD_WEBHOOK_URL) break;
-      await axios.post(DISCORD_WEBHOOK_URL, message);
-    }
-    return NextResponse.json<apiRes>({ message: 'success', data: assignments }, { status: 200 });
+    return NextResponse.json<apiRes>({ message: 'success', data: now }, { status: 200 });
   });
