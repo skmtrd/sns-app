b857b38e92c22adbbb2e795ee6e3da6788786428
skmtrd <skmtgame23@gmail.com>
2024-12-15 23:18:51 +0900

    Refactor SideBar component to improve navigation structure. Split navItems into separate arrays for better organization, and add a new profile link with an icon. Introduce visual separators between navigation sections for enhanced UI clarity.

--- src/components/layout/SideBar.tsx
+++ src/components/layout/SideBar.tsx
@@ -27,13 +27,23 @@ const SideBar = async () => {
     { page: '/timeline/all', label: 'タイムライン', icon: <Home /> },
     { page: '/assignmentshare/all', label: '課題共有', icon: <ClipboardList /> },
     { page: '/question/all', label: '質問スペース', icon: <HelpCircle /> },
-    { page: '/likes', label: 'いいねしたポスト', icon: <BookHeart /> },
-    { page: '/my-assignments', label: '登録した課題', icon: <FolderClock /> },
-    { page: '/bookmarks', label: 'ブックマークした質問', icon: <BookMarked /> },
-    { page: `/profile/${userId}`, label: 'プロフィール', icon: <CircleUser /> },
   ];
 
   const navItems2: NavItem[] = [
+    { page: '/likes', label: 'いいねしたポスト', icon: <BookHeart /> },
+    { page: '/my-assignments', label: '登録した課題', icon: <FolderClock /> },
+    { page: '/bookmarks', label: 'ブックマークした質問', icon: <BookMarked /> },
+  ];
+
+  const navItems3: NavItem[] = [
+    {
+      page: `/profile/${userId}`,
+      label: 'プロフィール',
+      icon: <CircleUser size={22} />,
+    },
+  ];
+
+  const postingItems: NavItem[] = [
     { page: '/post/new', label: 'ポスト', icon: <Antenna size={22} /> },
     { page: '/question/new', label: '質問', icon: <Hand size={22} /> },
     { page: '/assignment/new', label: '課題', icon: <FilePenLine size={22} /> },
@@ -44,10 +54,19 @@ const SideBar = async () => {
       <Link href={'/timeline/all'}>
         <h1 className='mb-4 hidden text-2xl font-bold text-blue-600 xl:block'>INIAD SNS</h1>
       </Link>
+
       {navItems1.map(({ page, label, icon: Icon }) => (
         <SidebarLinkingButton key={page} page={page} label={label} icon={Icon} />
       ))}
-      {navItems2.map(({ label, icon: Icon }) => (
+      <div className='my-[10px] h-[3px] w-full bg-gray-200' />
+      {navItems2.map(({ page, label, icon: Icon }) => (
+        <SidebarLinkingButton key={page} page={page} label={label} icon={Icon} />
+      ))}
+      <div className='my-[10px] h-[3px] w-full bg-gray-200' />
+      {navItems3.map(({ page, label, icon: Icon }) => (
+        <SidebarLinkingButton key={page} page={page} label={label} icon={Icon} />
+      ))}
+      {postingItems.map(({ label, icon: Icon }) => (
         <SidebarPostingButton key={label} label={label as 'ポスト' | '質問' | '課題'} icon={Icon} />
       ))}
     </div>
