17995a40070af301efb4e3e86b404ec8cebeb681
skmtrd <skmtgame23@gmail.com>
2024-12-23 14:56:15 +0900

    Fix typo in filteredQuestions mapping on QuestionPage component

--- src/components/question/QuestionPage.tsx
+++ src/components/question/QuestionPage.tsx
@@ -44,7 +44,7 @@ const QuestionPage: React.FC<QuestionPageProps> = ({
       />
       <div className='mx-auto mt-16 w-full max-w-5xl py-8 sm:px-6 lg:px-8'>
         <div className='flex flex-col items-center space-y-6'>
-          {filteredQuestisons.map((question: Question) => (
+          {filteredQuestions.map((question: Question) => (
             <QuestionPost key={question.id} question={question} currentUserId={currentUserId} />
           ))}
         </div>
