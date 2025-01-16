f03ed52c105de2126da410fb963782c5ca973934
skmtrd <skmtgame23@gmail.com>
2025-01-16 15:15:44 +0900

    ページコンポーネントのProps型をPagePropsに変更し、generateMetadataおよびTimelineAll関数の引数を簡素化

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,21 +1,21 @@
 import { getSession } from '@/app/actions/getSession';
 import { getSpecificPost } from '@/app/actions/getSpecificPost';
 import SpecificPostPage from '@/components/timeline/ReplyElement/SpecificPostPage';
-type Props = {
+type PageProps = {
   params: {
     id: string;
   };
+  searchParams: { [key: string]: string | string[] | undefined };
 };
-export const generateMetadata = async (props: Props) => {
-  const id = props.params.id;
+export const generateMetadata = async ({ params }: PageProps) => {
+  const id = params.id;
   const post = await getSpecificPost(id);
   return {
     title: `${post.author.name}のポスト / INIAD`,
   };
 };
 
-const TimelineAll = async (props: Props) => {
-  const params = props.params;
+const TimelineAll = async ({ params }: PageProps) => {
   const [session, post] = await Promise.all([getSession(), getSpecificPost(params.id)]);
 
   return <SpecificPostPage currentUserId={session.id} post={post} />;
