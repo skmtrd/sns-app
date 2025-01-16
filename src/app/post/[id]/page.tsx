c503b22a311ea580af32007d587303fb4a6c061f
skmtrd <skmtgame23@gmail.com>
2025-01-16 15:10:52 +0900

    ページコンポーネントのgenerateMetadataおよびTimelineAll関数の引数を型定義を用いて明確化

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,16 +1,22 @@
 import { getSession } from '@/app/actions/getSession';
 import { getSpecificPost } from '@/app/actions/getSpecificPost';
 import SpecificPostPage from '@/components/timeline/ReplyElement/SpecificPostPage';
-
-export const generateMetadata = async ({ params: { id } }: { params: { id: string } }) => {
+type Props = {
+  params: {
+    id: string;
+  };
+};
+export const generateMetadata = async (props: Props) => {
+  const id = props.params.id;
   const post = await getSpecificPost(id);
   return {
     title: `${post.author.name}のポスト / INIAD`,
   };
 };
 
-const TimelineAll = async ({ params: { id } }: { params: { id: string } }) => {
-  const [session, post] = await Promise.all([getSession(), getSpecificPost(id)]);
+const TimelineAll = async (props: Props) => {
+  const params = props.params;
+  const [session, post] = await Promise.all([getSession(), getSpecificPost(params.id)]);
 
   return <SpecificPostPage currentUserId={session.id} post={post} />;
 };
